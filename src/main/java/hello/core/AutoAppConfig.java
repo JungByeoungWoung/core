9c371d96b91701761f77bfb2be1dc40941efffc0
JungByeoungWoung <jbw03456@naver.com>
2021-06-12 00:38:34 +0900

    basePackages를 이용한 컴포넌트 스캔 위치와 기본 스캔 대상 설정하는 코드 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,10 +7,13 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component 라는 어노테이션을 붙은 클래스들을 스프링으로 등록 해줌
 @ComponentScan(
-        //컴포넌트 스캔으로 스프링 빈을 등록해주는데 그중에서
-        //뺄 것들을 등록해주는 역할
-        //Appconfig가 Configuration 어노테이션이 붙어있기 때문에 충돌 방지로 빼게 해줌
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //패키지 경로를 입력해주면 해당 패키지 안에 있는 Component 어노테이션이 붙은 클래스만
+        //빈 등록
+        basePackages = "hello.core.member",
+    //컴포넌트 스캔으로 스프링 빈을 등록해주는데 그중에서
+    //뺄 것들을 등록해주는 역할
+    //Appconfig가 Configuration 어노테이션이 붙어있기 때문에 충돌 방지로 빼게 해줌
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
