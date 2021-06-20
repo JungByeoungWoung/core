1a8796515e8485bb9636322534cd9d277f612247
JungByeoungWoung <jbw03456@naver.com>
2021-06-20 17:18:20 +0900

    request 스코프와 ObjectProvider<>를 사용하지 않고 @Scope(value = "request",proxyMode = ScopedProxyMode.TARGET_CLASS) 프록시 옵션을 사용해서 코드 작성

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +12,7 @@ import java.util.UUID;
 //requset 스코프로 지정
 //스코프 생성으로 인해 이 MyLogger 빈은 HTTP 요청 당 하나씩 생성되고,
 //HTTP 요청이 끝나는 시점에 소멸
-@Scope(value = "request")
+@Scope(value = "request",proxyMode = ScopedProxyMode.TARGET_CLASS)
 //로그를 출력하기 위한 클래스
 public class MyLogger {
     private String uuid;
